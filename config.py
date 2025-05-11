362c792838aa4fc120126b89c43757413696c0bb
Ethan Irizarry <ethan.irizarry@gmail.com>
2025-05-11 14:55:20 -0700

    Initial commit of Flask project

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:2JVBgN6fdZEi@ep-small-cell-a48v87ie-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # or another method to set a secret key
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_ZFUJzHv3iDK8@ep-silent-poetry-a4mzoyjf-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
